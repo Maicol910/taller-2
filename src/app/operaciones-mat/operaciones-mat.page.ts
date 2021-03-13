d2279e199f9c7a96d9531dd9e6c7fd467e613807
maicol <maicoljosa910@gmail.com>
2021-03-13 12:02:57 -0500

    2do punto

--- src/app/operaciones-mat/operaciones-mat.page.ts
+++ src/app/operaciones-mat/operaciones-mat.page.ts
@@ -8,7 +8,23 @@ import { Component, OnInit } from '@angular/core';
 export class OperacionesMatPage implements OnInit {
 
   constructor() { }
+  numero1=null;
+  operacion=null;
+  numero2=null;
+  resultado=null;
 
+  resultado2=[];
+
+  operaciones(){
+    if(this.operacion =='/'){
+      let resultado = this.numero1/this.numero2
+      console.log(resultado)
+    }if(this.operacion =='*'){
+      let resultado = this.numero1*this.numero2
+      console.log(resultado)
+    }
+  }
+ 
   ngOnInit() {
   }
 
