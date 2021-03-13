7425964d7214dfdb9700d38745ccc1c21faed7c3
maicol <maicoljosa910@gmail.com>
2021-03-13 12:18:31 -0500

    2do punto

--- src/app/operaciones-mat/operaciones-mat.page.ts
+++ src/app/operaciones-mat/operaciones-mat.page.ts
@@ -6,22 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./operaciones-mat.page.scss'],
 })
 export class OperacionesMatPage implements OnInit {
-
-  constructor() { }
   numero1=null;
   operacion=null;
   numero2=null;
   resultado=null;
 
-  resultado2=[];
+  constructor() { }
 
   operaciones(){
     if(this.operacion =='/'){
-      let resultado = this.numero1/this.numero2
-      console.log(resultado)
+      this.resultado = this.numero1/this.numero2
+      console.log(this.resultado)
     }if(this.operacion =='*'){
-      let resultado = this.numero1*this.numero2
-      console.log(resultado)
+      this.resultado = this.numero1*this.numero2
+      console.log(this.resultado)
+    }if(this.operacion =='-'){
+      this.resultado = this.numero1-this.numero2
+      console.log(this.resultado)
+    }if(this.operacion =='+'){
+      this.resultado= parseInt(this.numero1)+parseInt(this.numero2)
+      console.log(this.resultado)
     }
   }
  
