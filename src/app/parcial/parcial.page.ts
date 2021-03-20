59b96015229b3c8a0e6f45ec970b92a899a33f04
maicol <maicoljosa910@gmail.com>
2021-03-20 11:57:00 -0500

    funcina las operaciones

--- src/app/parcial/parcial.page.ts
+++ src/app/parcial/parcial.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-parcial',
@@ -7,8 +7,38 @@ import { NavController } from 'ionic-angular';
   styleUrls: ['./parcial.page.scss'],
 })
 export class ParcialPage implements OnInit {
-  constructor(public navCtrl: NavController) { }
+  constructor(public navCtrl: NavController) {
+  }  
   
+  resultado=null;
+  bandera=null;
+  datos=[]
+ 
+  numero1(numero1){
+    this.datos.push(numero1)
+    if(this.bandera==0){
+      this.bandera=1
+      this.resultado=numero1
+    }else
+    this.resultado+= numero1
+    console.log(this.datos)
+  }
+  
+  operaciones(simbol){
+    this.datos.push(simbol)
+    this.bandera=null
+  }
+
+  eliminar(){
+    this.datos=[]
+    this.resultado=null
+  }
+
+  resultadoFinal(){
+    this.resultado=eval(this.datos.join(''))
+    this.bandera=null
+  }
+
   ngOnInit() {
   }
 
